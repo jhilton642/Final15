4fbb5f3202476e704483e7f3e09694a03c4b765d
jhilton642 <joshua.hilton@att.net>
2020-10-26 14:19:24 -0500

    Add project files.

---
+++ Final15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Final15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Program.SayHello("German");
+            Console.WriteLine(x);
+        }
+        public static string SayHello(string language)
+        {
+            string translated ="";
+            switch (language)
+            {
+                case "German":
+                    translated = "Guten Tagge";
+                    break;
+                case "Dutch":
+                    translated = "Gooie Morgen";
+                        break;
+                case "Spanish":
+                    translated = "Hola";
+                    break;
+                case "French":
+                    translated = "Bonjour";
+                    break;
+                case "Russian":
+                    translated = "Здравствуйте";
+                    break;
+            }
+            return translated;
+        }
+    }
+}
